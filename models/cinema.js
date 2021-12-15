c47354d863baf65944dd5b0ae0b7dd7fe1a5e0ac
Marijke Drenth <drenth.marijke@googlemail.com>
2021-12-15 16:14:41 +0200

    Added get all titles and find by title methods

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,14 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.getAllTitles = function (){
+  const allTitles = this.films.map( film => film.title);
+  return allTitles;
+}
+
+Cinema.prototype.findByTitle = function (title) {
+  const filmObject = this.films.find(film => film.title === title);
+  return filmObject;
+}
+
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -28,8 +28,14 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, films);
   });
 
-  it('should be able to get a list of film titles');
-  it('should be able to find a film by title');
+  it('should be able to get a list of film titles', function () {
+    const actual = cinema.getAllTitles();
+    assert.deepStrictEqual(actual, ['Moonlight', 'Blade Runner 2049', 'Dunkirk', 'Black Panther', 'T2 Trainspotting'])
+  });
+  it('should be able to find a film by title', function () {
+    const actual = cinema.findByTitle("Blade Runner 2049");
+    assert.deepStrictEqual(actual, bladeRunner);
+  });
   it('should be able to filter films by genre');
   it('should be able to check whether there are some films from a particular year');
   it('should be able to check whether there are no films from a particular year');
